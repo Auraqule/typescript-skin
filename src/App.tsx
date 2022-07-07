b6d1df4bd7121bea5d07bb67c20b032a30589201
auraqule <auraqule@gmail.com>
2022-07-07 17:34:35 +0100

    react + typescript

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import List from "./components/List";
+import AddToList from "./components/AddToList";
+
+export interface IState {
+  people: {
+    name: string;
+    age: number;
+    img: string;
+    note?: string;
+  }[];
+}
+
+function App() {
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "LeBron James",
+      age: 35,
+      img: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+      note: "Allegeric to staying on the same team",
+    },
+    {
+      name: "Kobe Bryant",
+      age: 42,
+      img: "https://fullpresscoverage.com/wp-content/uploads/2020/01/101524695-457220551.jpg",
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <h1>People Invited to my Party</h1>
+      <List people={people} />
+      <AddToList setPeople={setPeople} people={people} />
+    </div>
+  );
+}
+
+export default App;
